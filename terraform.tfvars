e6347dc1ed24eb5a8c0c76dcc263f0e55ee00527
Sai-Git97 <gsaichandra2@gmail.com>
2025-06-20 08:18:23 -0400

    ganithivpc

---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+cidr_block = "10.70.0.0/16"
+vpc_name = "ganithi-vpc"
+cidr_block_publicsubnets = ["10.70.1.0/24","10.70.2.0/24","10.70.3.0/24"]
+azs = ["us-east-1a","us-east-1b","us-east-1c"]
+cidr_block_privatesubnets = ["10.70.5.0/24","10.70.7.0/24","10.70.9.0/24"]
---
+++ vpc.tf
@@ -0,0 +1,16 @@
+resource "aws_vpc" "ganithi" {
+    cidr_block = var.cidr_block  
+    enable_dns_hostnames = true
+    tags = {
+        Name = "${var.vpc_name}"
+    }
+}
+
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.ganithi.id
+    tags = {
+        Name = "${var.vpc_name}-igw"
+    }
+  
+}
